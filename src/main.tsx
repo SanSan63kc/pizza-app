d6d22998f3702f209149b9bc322ad94a2004c13f
SanSan63kc <Reagent-163-RW-08@yandex.ru>
2024-07-31 17:54:57 +0400

    урок 87

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Menu } from './pages/Menu/Menu.tsx'
+import { Cart } from './pages/Cart/Cart.tsx'
+import { Error } from './pages/Error/Error.tsx'
+import { Layout } from './layout/Menu/Menu.tsx'
+
+let router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Menu />,
+      },
+      {
+        path: "*",
+        element: <Cart />
+      }
+    ]
+  },
+  {
+    path: "*",
+    element: <Error />
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
